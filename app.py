6cf4782fa256b456f2aed35f1706b63e7f5bcad2
Chris Kim <chris.kim@algorand.com>
2023-05-15 12:55:09 -0700

    changed If to Assert

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ def asa_balance_with_reference_type(
     asset_balance = AssetHolding.balance(account.address(), asa.asset_id())
     return Seq(
         asset_balance,
-        If(asset_balance.hasValue())
-        .Then(output.set(asset_balance.value()))
-        .Else(Reject()),
+        Assert(asset_balance.hasValue()),
+        output.set(asset_balance.value()),
     )
 
 
@@ -22,9 +21,8 @@ def asa_balance_with_slot_referencing(*, output: abi.Uint64) -> Expr:
     asset_balance = AssetHolding.balance(Txn.accounts[0], Txn.assets[0])
     return Seq(
         asset_balance,
-        If(asset_balance.hasValue())
-        .Then(output.set(asset_balance.value()))
-        .Else(Reject()),
+        Assert(asset_balance.hasValue()),
+        output.set(asset_balance.value()),
     )
 
 
